1b7c2e1314c8cdfd55492504c987bfeef4bdd1f4
Leonardo Oberle <leocello@gmail.com>
2020-09-30 15:37:55 +1300

    ...

--- 2020-09-30/solution.test.js
+++ 2020-09-30/solution.test.js
@@ -1,6 +1,6 @@
 const solution = require('./solution.js');
 
-test('2020-09-30 - Simple tests', () => {
+it('2020-09-30 - Simple tests', () => {
     expect(solution([10, 15, 3, 7], 17)).toBe(true);
     //               10    +    7 = 17
     
